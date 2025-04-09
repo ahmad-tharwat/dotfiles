bfe18a95a1982550e9c98dc5f89688d14b898aa1
ahmad <ahmad_20th@tuta.io>
2025-04-09 16:16:02 +0200

    Enabled bluetooth for bcm43142

--- .local/bin/bcm43142.sh
+++ .local/bin/bcm43142.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-apt-get update
-apt-get install linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms
+### Enable Wi-Fi
+apt update
+apt install linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms
 modprobe -r b44 b43 b43legacy ssb brcmsmac bcma
 modprobe wl
+
+### Enable Bluetooth
+curl -H 'Accept: application/vnd.github.v3.raw' --output-dir /lib/firmware/brcm -O -L "https://api.github.com/repos/winterheart/broadcom-bt-firmware/contents/brcm/BCM43142A0-0a5c-21d7.hcd"
+systemctl enable bluetooth
+
+echo "------------------------------------------------------"
+echo "You need to restart the computer for bluetooth to work"
+echo "------------------------------------------------------"
 
