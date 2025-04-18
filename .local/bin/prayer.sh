dc52b7d087e415dec278ea96cbdd5fd376a2be65
ahmad <ahmad_20th@tuta.io>
2025-04-18 10:45:30 +0200

    refined and made POSIX compliant

--- .local/bin/prayer.sh
+++ .local/bin/prayer.sh
@@ -1,61 +1,69 @@
-#!/bin/bash
+#!/bin/sh
+
+# TODO
+# [x] make defaults
+# [ ] store result locally
+# [ ] format output
+# [ ] play azhan
+# [ ] background process
+# [ ] ensure POSIX
 
 flags()
 {
-	printf "Default:\n"
-	printf "  prayer.sh\n\n"
+	printf "Default: %s\n\n" "$(basename "$0")"
 	printf "Options:\n"
-	printf "  -h\t show the options\n\n"
-	printf "  -d\t date in DD-MM-YYYY format (defaults to current system date)\n"
-	printf "  -c\t city\n"
-	printf "  -r\t country code\n"
-	printf "  -l\t latitudeAdjustmentMethod\n"
-	printf "  -m\t method\n"
+	printf "  -c	city\n"
+	printf "  -d	date in DD-MM-YYYY format (defaults to current system date)\n"
+	printf "  -h	show the options\n"
+	printf "  -l	latitude Adjustment Method\n"
+	printf "  -m	method\n"
+	printf "  -r	country code\n"
 }
 
-printf -v date '%(%d-%m-%Y)T' -1	# do not change this date format; this is for the url to work
 
-if [ $# -eq 0 ]; then
-	# my default values to use for the script. 
-	# you will probably want to change them
-	city="Alexandria"
-	country="EG"
-	method=5
-	latitudeAdjustmentMethod=3
-else
-	while getopts ":hc:d:l:m:r:" option; do
-		case $option in
-			c)
-				city=${OPTARG};;
-			d)
-				date=${OPTARG};;
-			l)
-				latitudeAdjustmentMethod=${OPTARG};;
-			m)
-				method=${OPTARG};;
-			r)
-				country=${OPTARG};;
-			h)
-				flags
-				exit;;
-			:)
-				printf "Option -${OPTARG} requires an argument.\n"
-				exit;;
-			\?)
-				printf "Invalid option: -${OPTARG}\n"
-				exit 1;;
-		esac
-	done
+while getopts ":hc:d:l:m:r:" option; do
+	case ${option} in
+		c)
+			city=${OPTARG};;
+		d)
+			date=${OPTARG};;
+		l)
+			lat=${OPTARG};;
+		m)
+			method=${OPTARG};;
+		r)
+			country=${OPTARG};;
+		h)
+			flags
+			exit;;
+		:)
+			printf "Option -%s requires an argument.\n" "${OPTARG}" >&2
+			exit;;
+		\?)
+			printf "Invalid option: -%s\n" "${OPTARG}" >&2
+			exit 1;;
+		*)
+			exit 1;;
+	esac
+done
 	
+
+# default values to use for the script. You will probably want to change them
+if [ "${OPTIND}" -eq 1 ]; then
+	city=${city:-"Alexandria"}
+	country=${country:-"EG"}
+	lat=${lat:-3}
+	method=${method:-5}
+	date=${date:-$(date +%d-%m-%Y)}	# do not change this date format; this is for the url to work
 fi
 
-if [ -z "$city" ] || [ -z "$country" ] || [ -z "$method" ] || [ -z "$latitudeAdjustmentMethod" ]; then
-	printf "Error: did not specify arguments!\n\n"
-	printf "Usage: $0 [options] [arguments]\n"
-	flags
+
+if [ -z "${city}" ] || [ -z "${country}" ] || [ -z "${method}" ] || [ -z "${lat}" ] || [ -z "${date}" ]; then
+	printf "Error: did not specify arguments!\n" >&2
+	printf "Usage: %s [options] [arguments]\n" "$(basename "$0")"
 	exit 1
 else
-	res=`curl -X GET "https://api.aladhan.com/v1/timingsByCity/${date}?city=${city}&country=${country}&method=${method}&latitudeAdjustmentMethod=${latitudeAdjustmentMethod}" -H 'accept: application/json'`
-	printf "$res" | jq .data.timings
+	res=$(curl -X GET "https://api.aladhan.com/v1/timingsByCity/${date}?city=${city}&country=${country}&method=${method}&latitudeAdjustmentMethod=${lat}" -H 'accept: application/json')
+	printf "%s" "${res}" | jq .data.timings
 fi
 
